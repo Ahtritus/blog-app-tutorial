8adf2a0c4ce699893951b43e0b4ddd8dbd320d78
Ahtritus <sutirthabharatidas@gmail.com>
2022-01-15 13:17:12 +0530

    first commit, created server and started working on db

---
+++ node-blog-server/config/db.config.js
@@ -0,0 +1,7 @@
+//creating connection for mysql
+module.exports = {
+  host: "localhost",
+  user: "root",
+  password: "abcd1234",
+  database: "blogDB",
+};
---
+++ node-blog-server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ encoded: true }));
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.json("Welcome to the backend server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
